53050f5779c54ea29f3352859be2ead2ec6e5ec2
Adipramana <herryprasetyo.dev@gmail.com>
2025-08-13 07:42:22 +0700

    edit category page

---
+++ resources/views/categories/edit.blade.php
@@ -0,0 +1,35 @@
+@extends('master')
+
+@section('title')
+    <title>
+        Category
+    </title>
+@endsection
+
+@section('content')
+    <section class="section">
+        <div class="section-header">
+            <h1>Edit Category</h1>
+        </div>
+        <div class="section-body">
+            <div class="card">
+                <div class="card-header">
+                    <h4>Edit Category</h4>
+                </div>
+                <div class="card-body">
+                    <form action="{{ route('category.update', $category->id) }}" method="POST">
+                        @csrf
+                        @method('PUT')
+                        <div class="form-group">
+                            <label for="name">Category Name</label>
+                            <input type="text" class="form-control" id="name" name="name"
+                                value="{{ $category->name }}" required>
+                        </div>
+
+                        <button type="submit" class="btn btn-primary">Update Category</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </section>
+@endsection
